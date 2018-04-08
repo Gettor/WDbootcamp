b8e006e725e6716e8c2316abfb9903619d2cee20
Gettor <sdobroc@gmail.com>
2018-04-08 21:48:45 +0200

    Healthbar demo

---
+++ healthbar/script.js
@@ -0,0 +1,48 @@
+var maxHealth = 40;
+var maxWidthConst = 190;
+var healthColors = [
+  "#3EC072FF",
+  "#BEAC40FF",
+  "#DE3020FF"
+];
+
+
+function getRandomHit() {
+  return maxHealth*(Math.floor(Math.random() * 8) + 1);
+}
+
+function onHit(barId) {
+  var boxOne = document.getElementsByClassName('bar')[barId];
+  var step = getRandomHit();
+  var curWidth = boxOne.getAttribute("curValue") - step;
+  if (curWidth < 0){
+    curWidth = 0;
+  }
+  var maxWidthFromProperty = boxOne.getAttribute("maxValue");
+  boxOne.setAttribute("curValue", curWidth);
+  var percentage = (curWidth / maxWidthFromProperty);
+  var newWidth = percentage * maxWidthConst;
+  var display = newWidth.toString().concat("px");
+  boxOne.style.width = display;
+
+  if (percentage < 0.7)
+    boxOne.style.backgroundColor = healthColors[1];
+  if (percentage < 0.2)
+    boxOne.style.backgroundColor = healthColors[2];
+
+  var hpText = document.getElementsByClassName('healthText')[barId];
+  hpText.innerHTML = Math.floor((percentage*maxHealth)).toString().concat("/").concat(maxHealth.toString());
+
+}
+
+function onReset(barId) {
+  var boxOne = document.getElementsByClassName('bar')[barId];
+  var maxWidthFromProperty = boxOne.getAttribute("maxValue");
+  boxOne.setAttribute("curValue", maxWidthFromProperty);
+  boxOne.style.width = maxWidthConst.toString().concat("px");
+  boxOne.style.backgroundColor = healthColors[0];
+
+  var hpText = document.getElementsByClassName('healthText')[barId];
+  hpText.innerHTML = maxHealth.toString().concat("/").concat(maxHealth.toString());
+
+}
